a9e097edc85b784a0c1313a292d5cd33678ead59
EC2 Default User <ec2-user@ip-172-31-43-130.ap-south-1.compute.internal>
2024-07-06 04:48:24 +0000

    added in master branch

---
+++ script.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+mkdir India
+
+mkdir India/Telangana
+mkdir India/Telangana/Hyderabad
+cd India/Telangana/Hyderabad
+touch {a..c}.txt
+cd ../../../
+mkdir India/Telangana/Suryapet
+cd India/Telangana/Suryapet
+touch {a..c}.txt
+cd ../../../
+mkdir India/Telangana/Nalgonda
+cd India/Telangana/Nalgonda
+touch {a..c}.txt
+cd ../../../
+
+mkdir India/Andhrapradesh
+mkdir India/Andhrapradesh/Vijayawada
+cd India/Andhrapradesh/Vijayawada
+touch {a..c}.txt
+cd ../../../
+mkdir India/Andhrapradesh/WestGodavari
+cd India/Andhrapradesh/WestGodavari
+touch {a..c}.txt
+cd ../../../
+mkdir India/Andhrapradesh/EestGodavari
+cd India/Andhrapradesh/EestGodavari
+touch {a..c}.txt
+cd ../../../
+
+mkdir India/Karnataka
+mkdir India/Karnataka/Bangalore
+cd India/Karnataka/Bangalore
+touch {a..c}.txt
+cd ../../../
+mkdir India/Karnataka/Mysore
+cd India/Karnataka/Mysore
+touch {a..c}.txt
+cd ../../../
+mkdir India/Karnataka/Hubli
+cd India/Karnataka/Hubli
